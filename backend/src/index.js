9afbfbd5264b7ac8bc6d5ced550476fd86b3a0cb
Krrish-Devani <krrishdevani84@gmail.com>
2025-07-14 22:25:27 +0530

    NexChat

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 import connectDB from './lib/mongodb.js';
 
@@ -16,8 +15,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
@@ -31,11 +29,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-    app.get('*', (req, res) => {
-        if (!req.path.startsWith('/api')) {
-            res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
-        }
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get(/(.*)/, (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     })
 }
 
